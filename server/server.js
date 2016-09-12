cb5e8779b68575d956b711e241c4b0702aaa2afb
Pomax <pomax@nihongoresources.com>
2016-09-12 14:55:12 -0700

    .

--- server/server.js
+++ server/server.js
@@ -12,18 +12,56 @@ app.get('/socket.io.js', function(req, res) {
   });
 });
 
-io.on('connection', function(socket) {
-  console.log('a user connected');
+var snakeids = [];
 
+// A player joins the game
+io.on('connection', function(socket) {
+
+  // welcome user, send them their snake color (may be user-changeble later)
+  console.log('a user connected');  
+  socket.emit('welcome', {
+    color: "red"
+  });
+
+  /*
+    Client-generated events
+  */
+
+  // a client disconnects
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 
-  // pass-through for now
+  // record a snake's movement
   socket.on('direction', function(data){
   	console.log("directional change: ", JSON.stringify(data));;
-    io.emit('direction', data);
+    io.emit('direction', {
+      id: data.id,
+      direction: data.direction
+    });
   });
+
+  // client requests a new snake
+  socket.on('make-snake', function() {
+  	console.log("new snake request");
+  	var id = snakeids.length;
+  	snakeids[id] = socket;
+  	var data = {
+      id: id,
+      x: (Math.random() * 40)  | 0,
+      y: (Math.random() * 40)  | 0,
+      lenght: 3
+    };
+  	console.log("new snake request", JSON.stringify(data));
+    io.emit('spawn-snake', data);
+  });
+
+  /*
+    Server-generated events
+
+    'kill-snake': id
+    'disconnect-snake': id
+  */
 });
 
 
